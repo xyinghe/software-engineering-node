ee3edeb8caf097da3dd79a9e0720a4e932cf3530
xyinghe <he.xuey@northeastern.edu>
2022-02-04 18:27:41 -0500

    first commit

---
+++ server.ts
@@ -0,0 +1,11 @@
+import express, {Request, Response} from 'express';
+const app = express();
+
+app.get('/hello', (req: Request, res: Response) =>
+    res.send('Hello World!'));
+
+app.get('/add/:a/:b', (req: Request, res: Response) =>
+    res.send(req.params.a + req.params.b));
+
+const PORT = 4000;
+app.listen(process.env.PORT || PORT);
