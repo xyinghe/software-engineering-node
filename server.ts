eaaa1b78b2c1b6af4adc5afc740951dee7fe50c4
xyinghe <he.xuey@northeastern.edu>
2022-02-08 02:23:42 -0500

    connect to mongoDB

--- server.ts
+++ server.ts
@@ -1,11 +1,30 @@
-import express, {Request, Response} from 'express';
-const app = express();
+import express,{Request, Response} from 'express';
+import dotenv from "dotenv"
+import UserController from "./controllers/UserController";
+import TuitController from "./controllers/TuitController";
+import mongoose from "mongoose";
 
-app.get('/hello', (req: Request, res: Response) =>
-    res.send('Hello World!'));
+// connect to the database
+dotenv.config();
+const DB_USERNAME = process.env.DB_USERNAME;
+const DB_PASSWORD = process.env.DB_PASSWORD;
+const connectionString = `mongodb+srv://${DB_USERNAME}:${DB_PASSWORD}@cluster0.vu2ks.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+mongoose.connect(connectionString);
+
+// mongoose.connect('mongodb://localhost:27017/users-db');
+// create RESTful Web service API
+const app = express();
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) =>
+    res.send('Welcome!'));
 
 app.get('/add/:a/:b', (req: Request, res: Response) =>
     res.send(req.params.a + req.params.b));
+
+const userController = UserController.getInstance(app);
+const tuitController = TuitController.getInstance(app);
+
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);
